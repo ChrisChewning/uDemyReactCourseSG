68bb5a4d6c932b7be5ad20910958494f0f16470d
Chris <chewning.cl@gmail.com>
2018-12-12 22:01:56 -0600

    fixed console errors & made notes

--- components/ImageList.js
+++ components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageList = (props) => {
   const images = props.images.map((image) => { //called w/ each image from imgs.
-    return <img src={image.urls.regular} /> //returns long url
+    return <img alt = {image.description} key = {image.id} src={image.urls.regular} /> //returns long url
   })
 
   return <div>{images}</div>
@@ -20,3 +20,6 @@ export default ImageList;
 //     return <img src={image.urls.regular} /> //returns long url
 //   })
 // };
+
+
+// NOTE 2: you can replace (image) as your argument in map and instead, say description, id, urls.regular. Then make drop image off each of the properties in return so it's destructured.
