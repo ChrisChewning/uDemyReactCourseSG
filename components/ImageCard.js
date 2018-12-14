ae2b3b536de32fa78361ee73331331861296eb43
Chris <chewning.cl@gmail.com>
2018-12-13 22:17:46 -0600

    ref and destructuring finished

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -9,17 +9,28 @@ class ImageCard extends React.Component {
 
     //REF: gives direct access to a single DOM element. in constructor assign as variable and use as props. no need for state b.c refs will not change over time.
     //in this case, it reaches into the DOM & figures out the img height.
-    
-    this.imageRef = React.createRef();
 
+    this.imageRef = React.createRef();
   }
+
+  componentDidMount(){ //callback function b.c logging this.imageRefu.current.clientHeight returned 0 since it logged before img could load.
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+
 
   render(){
     const { description, urls} = this.props.image;
     return (
       <div>
         <img
-          alt={description} src={urls.regular}
+          ref={this.imageRef}
+          alt={description}
+          src={urls.regular}
 
           //desctructured ^
           // alt={this.props.image.description}
