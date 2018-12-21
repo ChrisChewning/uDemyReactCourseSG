3812297f89d4a8367c780ceb3a755fcdeecfda55
Chris <chewning.cl@gmail.com>
2018-12-20 22:01:30 -0600

    rendering image lists finished. project 3 done

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -7,6 +7,8 @@ class ImageCard extends React.Component {
   constructor(props){
     super(props);
 
+    this.state = { spans: 0 };
+
     //REF: gives direct access to a single DOM element. in constructor assign as variable and use as props. no need for state b.c refs will not change over time.
     //in this case, it reaches into the DOM & figures out the img height.
 
@@ -17,8 +19,14 @@ class ImageCard extends React.Component {
     this.imageRef.current.addEventListener('load', this.setSpans);
   }
 
-  
+
   setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans }) //could just say spans
+
     console.log(this.imageRef.current.clientHeight);
   }
 
@@ -26,7 +34,7 @@ class ImageCard extends React.Component {
   render(){
     const { description, urls} = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
           ref={this.imageRef}
           alt={description}
